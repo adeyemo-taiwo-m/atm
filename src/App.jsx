41d1302eb761030a0d41b1f4ed0f90087d89b77d
Adeyemo Taiwo M <adeyemotaiwom999@gmail>
2025-11-11 23:18:15 +0100

    deleted the unused files

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,12 @@ import Process from "./sections/Approach/Process";
 import Testimonials from "./sections/Testimonials/Testimonials";
 import Contact from "./sections/Contact section/Contact";
 import Header from "./ui/Header";
-import LightRays from "./ui/LightRays";
-import RippleGrid from "./ui/LightRays";
+
 import Booking from "./sections/booking/Booking";
-import MetallicLogo from "./ui/MovingLogo";
 
 function App() {
   return (
     <>
-      {/* <div
-        className=" w-screen"
-        style={{
-          width: "100%",
-          height: "700px",
-          position: "absolute",
-          top: 0,
-          right: 0,
-        }}
-      >
-        <LightRays
-          raysOrigin="top-center"
-          raysColor="#9999ff"
-          raysSpeed={1.5}
-          lightSpread={0.8}
-          rayLength={1.2}
-          followMouse={true}
-          mouseInfluence={0.1}
-          noiseAmount={0.1}
-          distortion={0.05}
-          className="custom-rays"
-        />
-      </div> */}
       <>
         <div className="px-4 relative  tab:px-8 lap:px-12 desk:px-20">
           <Header />
