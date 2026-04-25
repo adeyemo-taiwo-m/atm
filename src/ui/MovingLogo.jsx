d95954467e910168b2a1da2b9af62c185e6c2c9f
YourGitHubUsername <adeyemotaiwom999@gmail.com>
2026-04-25 13:36:26 +0100

    amended the booking section

--- src/ui/MovingLogo.jsx
+++ src/ui/MovingLogo.jsx
@@ -4,12 +4,12 @@ import { motion } from "framer-motion";
 
 function MovingLogo({ children }) {
   return (
-    <div className="relative w-full h-screen overflow-hidden ">
+    <div className="relative w-full min-h-screen overflow-x-hidden py-12 flex items-center justify-center">
       {/* Background moving text */}
       <motion.div
         className="absolute top-1/2 left-0 text-[10rem] font-bold scale-200 text-transparent bg-clip-text 
                    bg-gradient-to-r from-[var(--color-neutral-400)] to-[var(--color-neutral-800)] opacity-20"
-        style={{ transform: "translateY(-20%)" }}
+        style={{ transform: "translateY(-50%)" }}
         animate={{ x: ["-50%", "100%", "-50%"] }} // move left -> right -> left
         transition={{
           duration: 25,
@@ -21,7 +21,7 @@ function MovingLogo({ children }) {
       </motion.div>
 
       {/* Main content */}
-      <div className="relative z-10 flex justify-center items-center h-full">
+      <div className="relative z-10 w-full">
         {children}{" "}
       </div>
     </div>
